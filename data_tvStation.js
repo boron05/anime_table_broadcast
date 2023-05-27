8ea4f221329f898840407a184964b1e590dbe43d
boron05 <108002531+boron05@users.noreply.github.com>
2023-05-27 13:16:58 +0900

    Add files via upload

---
+++ data_tvStation.js
@@ -0,0 +1,764 @@
+var TV_STATIONS = {
+  "北海道放送": {
+    "type": "地上波",
+    "area": "北海道",
+    "group": "TBS",
+    "abbrev": "HBC"
+  },
+  "札幌テレビ": {
+    "type": "地上波",
+    "area": "北海道",
+    "group": "日本テレビ",
+    "abbrev": "STV"
+  },
+  "北海道テレビ": {
+    "type": "地上波",
+    "area": "北海道",
+    "group": "テレビ朝日",
+    "abbrev": "HTB"
+  },
+  "テレビ北海道": {
+    "type": "地上波",
+    "area": "北海道",
+    "group": "テレビ東京",
+    "abbrev": "TVh"
+  },
+  "北海道文化放送": {
+    "type": "地上波",
+    "area": "北海道",
+    "group": "フジテレビ",
+    "abbrev": "UHB"
+  },
+  "青森放送": {
+    "type": "地上波",
+    "area": "東北",
+    "group": "日本テレビ",
+    "abbrev": "RAB"
+  },
+  "青森朝日放送": {
+    "type": "地上波",
+    "area": "東北",
+    "group": "テレビ朝日",
+    "abbrev": "ABA"
+  },
+  "青森テレビ": {
+    "type": "地上波",
+    "area": "東北",
+    "group": "TBS",
+    "abbrev": "ATV"
+  },
+  "テレビ岩手": {
+    "type": "地上波",
+    "area": "東北",
+    "group": "日本テレビ",
+    "abbrev": "TVI"
+  },
+  "岩手朝日テレビ": {
+    "type": "地上波",
+    "area": "東北",
+    "group": "テレビ朝日",
+    "abbrev": "IAT"
+  },
+  "岩手放送": {
+    "type": "地上波",
+    "area": "東北",
+    "group": "TBS",
+    "abbrev": "IBC"
+  },
+  "岩手めんこいテレビ": {
+    "type": "地上波",
+    "area": "東北",
+    "group": "フジテレビ",
+    "abbrev": "mit"
+  },
+  "東北放送": {
+    "type": "地上波",
+    "area": "東北",
+    "group": "TBS",
+    "abbrev": "TBC"
+  },
+  "宮城テレビ放送（ミヤギテレビ）": {
+    "type": "地上波",
+    "area": "東北",
+    "group": "日本テレビ",
+    "abbrev": "MMT"
+  },
+  "東日本放送": {
+    "type": "地上波",
+    "area": "東北",
+    "group": "テレビ朝日",
+    "abbrev": "KHB"
+  },
+  "仙台放送": {
+    "type": "地上波",
+    "area": "東北",
+    "group": "フジテレビ",
+    "abbrev": "OX"
+  },
+  "秋田放送": {
+    "type": "地上波",
+    "area": "東北",
+    "group": "日本テレビ",
+    "abbrev": "ABS"
+  },
+  "秋田朝日放送": {
+    "type": "地上波",
+    "area": "東北",
+    "group": "テレビ朝日",
+    "abbrev": "AAB"
+  },
+  "秋田テレビ": {
+    "type": "地上波",
+    "area": "東北",
+    "group": "フジテレビ",
+    "abbrev": "AKT"
+  },
+  "山形放送": {
+    "type": "地上波",
+    "area": "東北",
+    "group": "日本テレビ",
+    "abbrev": "YBC"
+  },
+  "山形テレビ": {
+    "type": "地上波",
+    "area": "東北",
+    "group": "テレビ朝日",
+    "abbrev": "YTS"
+  },
+  "テレビユー山形": {
+    "type": "地上波",
+    "area": "東北",
+    "group": "TBS",
+    "abbrev": "TUY"
+  },
+  "さくらんぼテレビジョン": {
+    "type": "地上波",
+    "area": "東北",
+    "group": "フジテレビ",
+    "abbrev": "SAY"
+  },
+  "福島中央テレビ": {
+    "type": "地上波",
+    "area": "東北",
+    "group": "日本テレビ",
+    "abbrev": "FCT"
+  },
+  "福島放送": {
+    "type": "地上波",
+    "area": "東北",
+    "group": "テレビ朝日",
+    "abbrev": "KFB"
+  },
+  "テレビユー福島": {
+    "type": "地上波",
+    "area": "東北",
+    "group": "TBS",
+    "abbrev": "TUF"
+  },
+  "福島テレビ": {
+    "type": "地上波",
+    "area": "東北",
+    "group": "フジテレビ",
+    "abbrev": "FTV"
+  },
+  "NHK": {
+    "type": "地上波",
+    "area": "関東",
+    "group": "",
+    "abbrev": "NHK"
+  },
+  "日本テレビ": {
+    "type": "地上波",
+    "area": "関東",
+    "group": "日本テレビ",
+    "abbrev": "NTV"
+  },
+  "テレビ朝日": {
+    "type": "地上波",
+    "area": "関東",
+    "group": "テレビ朝日",
+    "abbrev": "EX"
+  },
+  "TBSテレビ": {
+    "type": "地上波",
+    "area": "関東",
+    "group": "TBS",
+    "abbrev": "TBS"
+  },
+  "テレビ東京": {
+    "type": "地上波",
+    "area": "関東",
+    "group": "テレビ東京",
+    "abbrev": "TX"
+  },
+  "フジテレビ": {
+    "type": "地上波",
+    "area": "関東",
+    "group": "フジテレビ",
+    "abbrev": "CX"
+  },
+  "TOKYO MX": {
+    "type": "地上波",
+    "area": "関東",
+    "group": "",
+    "abbrev": "MX"
+  },
+  "千葉テレビ（チバテレ）": {
+    "type": "地上波",
+    "area": "関東",
+    "group": "",
+    "abbrev": "CTC"
+  },
+  "テレビ神奈川": {
+    "type": "地上波",
+    "area": "関東",
+    "group": "",
+    "abbrev": "tvk"
+  },
+  "テレビ埼玉（テレ玉）": {
+    "type": "地上波",
+    "area": "関東",
+    "group": "",
+    "abbrev": "TVS"
+  },
+  "群馬テレビ": {
+    "type": "地上波",
+    "area": "関東",
+    "group": "",
+    "abbrev": "GTV"
+  },
+  "とちぎテレビ": {
+    "type": "地上波",
+    "area": "関東",
+    "group": "",
+    "abbrev": "GYT"
+  },
+  "山梨放送": {
+    "type": "地上波",
+    "area": "北陸・甲信越",
+    "group": "日本テレビ",
+    "abbrev": "YBS"
+  },
+  "テレビ山梨": {
+    "type": "地上波",
+    "area": "北陸・甲信越",
+    "group": "TBS",
+    "abbrev": "UTY"
+  },
+  "テレビ新潟": {
+    "type": "地上波",
+    "area": "北陸・甲信越",
+    "group": "日本テレビ",
+    "abbrev": "TeNY"
+  },
+  "新潟テレビ21": {
+    "type": "地上波",
+    "area": "北陸・甲信越",
+    "group": "テレビ朝日",
+    "abbrev": "UX"
+  },
+  "新潟放送": {
+    "type": "地上波",
+    "area": "北陸・甲信越",
+    "group": "TBS",
+    "abbrev": "BSN"
+  },
+  "新潟総合テレビ（Niigata Smile TV）": {
+    "type": "地上波",
+    "area": "北陸・甲信越",
+    "group": "フジテレビ",
+    "abbrev": "NST"
+  },
+  "テレビ信州": {
+    "type": "地上波",
+    "area": "北陸・甲信越",
+    "group": "日本テレビ",
+    "abbrev": "TSB"
+  },
+  "長野朝日放送": {
+    "type": "地上波",
+    "area": "北陸・甲信越",
+    "group": "テレビ朝日",
+    "abbrev": "abn"
+  },
+  "信越放送": {
+    "type": "地上波",
+    "area": "北陸・甲信越",
+    "group": "TBS",
+    "abbrev": "SBC"
+  },
+  "長野放送": {
+    "type": "地上波",
+    "area": "北陸・甲信越",
+    "group": "フジテレビ",
+    "abbrev": "NBS"
+  },
+  "北日本放送": {
+    "type": "地上波",
+    "area": "北陸・甲信越",
+    "group": "日本テレビ",
+    "abbrev": "KNB"
+  },
+  "チューリップテレビ": {
+    "type": "地上波",
+    "area": "北陸・甲信越",
+    "group": "TBS",
+    "abbrev": "TUT"
+  },
+  "富山テレビ放送": {
+    "type": "地上波",
+    "area": "北陸・甲信越",
+    "group": "フジテレビ",
+    "abbrev": "BBT"
+  },
+  "テレビ金沢": {
+    "type": "地上波",
+    "area": "北陸・甲信越",
+    "group": "日本テレビ",
+    "abbrev": "KTK"
+  },
+  "北陸朝日放送": {
+    "type": "地上波",
+    "area": "北陸・甲信越",
+    "group": "テレビ朝日",
+    "abbrev": "HAB"
+  },
+  "北陸放送": {
+    "type": "地上波",
+    "area": "北陸・甲信越",
+    "group": "TBS",
+    "abbrev": "MRO"
+  },
+  "石川テレビ": {
+    "type": "地上波",
+    "area": "北陸・甲信越",
+    "group": "フジテレビ",
+    "abbrev": "ITC"
+  },
+  "福井放送": {
+    "type": "地上波",
+    "area": "北陸・甲信越",
+    "group": "日本テレビ",
+    "abbrev": "FBC"
+  },
+  "福井テレビ": {
+    "type": "地上波",
+    "area": "北陸・甲信越",
+    "group": "フジテレビ",
+    "abbrev": "FTB"
+  },
+  "東海テレビ": {
+    "type": "地上波",
+    "area": "東海",
+    "group": "フジテレビ",
+    "abbrev": "THK"
+  },
+  "中京テレビ": {
+    "type": "地上波",
+    "area": "東海",
+    "group": "日本テレビ",
+    "abbrev": "CTV"
+  },
+  "CBCテレビ": {
+    "type": "地上波",
+    "area": "東海",
+    "group": "TBS",
+    "abbrev": "CBC"
+  },
+  "名古屋テレビ（メ～テレ）": {
+    "type": "地上波",
+    "area": "東海",
+    "group": "テレビ朝日",
+    "abbrev": "NBN"
+  },
+  "テレビ愛知": {
+    "type": "地上波",
+    "area": "東海",
+    "group": "テレビ東京",
+    "abbrev": "TVA"
+  },
+  "三重テレビ": {
+    "type": "地上波",
+    "area": "東海",
+    "group": "",
+    "abbrev": "MTV"
+  },
+  "岐阜放送（ぎふチャン）": {
+    "type": "地上波",
+    "area": "東海",
+    "group": "",
+    "abbrev": "GBS"
+  },
+  "静岡第一テレビ（Daiichi-TV）": {
+    "type": "地上波",
+    "area": "東海",
+    "group": "日本テレビ",
+    "abbrev": "SDT"
+  },
+  "静岡朝日テレビ（あさひテレビ）": {
+    "type": "地上波",
+    "area": "東海",
+    "group": "テレビ朝日",
+    "abbrev": "SATV"
+  },
+  "静岡放送": {
+    "type": "地上波",
+    "area": "東海",
+    "group": "TBS",
+    "abbrev": "SBS"
+  },
+  "テレビ静岡": {
+    "type": "地上波",
+    "area": "東海",
+    "group": "フジテレビ",
+    "abbrev": "SUT"
+  },
+  "毎日放送": {
+    "type": "地上波",
+    "area": "九州",
+    "group": "TBS",
+    "abbrev": "RKB"
+  },
+  "朝日放送": {
+    "type": "地上波",
+    "area": "関西",
+    "group": "テレビ朝日",
+    "abbrev": "ABC"
+  },
+  "テレビ大阪": {
+    "type": "地上波",
+    "area": "関西",
+    "group": "テレビ東京",
+    "abbrev": "TVO"
+  },
+  "関西テレビ": {
+    "type": "地上波",
+    "area": "関西",
+    "group": "フジテレビ",
+    "abbrev": "KTV"
+  },
+  "読売テレビ": {
+    "type": "地上波",
+    "area": "関西",
+    "group": "日本テレビ",
+    "abbrev": "ytv"
+  },
+  "京都放送": {
+    "type": "地上波",
+    "area": "関西",
+    "group": "",
+    "abbrev": "KBS"
+  },
+  "びわ湖放送": {
+    "type": "地上波",
+    "area": "関西",
+    "group": "",
+    "abbrev": "BBC"
+  },
+  "サンテレビ": {
+    "type": "地上波",
+    "area": "関西",
+    "group": "",
+    "abbrev": "SUN"
+  },
+  "奈良テレビ": {
+    "type": "地上波",
+    "area": "関西",
+    "group": "",
+    "abbrev": "TVN"
+  },
+  "テレビ和歌山": {
+    "type": "地上波",
+    "area": "関西",
+    "group": "",
+    "abbrev": "WTV"
+  },
+  "日本海テレビ": {
+    "type": "地上波",
+    "area": "中国",
+    "group": "日本テレビ",
+    "abbrev": "NKT"
+  },
+  "山陰放送": {
+    "type": "地上波",
+    "area": "中国",
+    "group": "TBS",
+    "abbrev": "BSS"
+  },
+  "山陰中央テレビ": {
+    "type": "地上波",
+    "area": "中国",
+    "group": "フジテレビ",
+    "abbrev": "TSK"
+  },
+  "西日本放送": {
+    "type": "地上波",
+    "area": "中国",
+    "group": "日本テレビ",
+    "abbrev": "RNC"
+  },
+  "瀬戸内海放送": {
+    "type": "地上波",
+    "area": "中国",
+    "group": "テレビ朝日",
+    "abbrev": "KSB"
+  },
+  "山陽放送": {
+    "type": "地上波",
+    "area": "中国",
+    "group": "TBS",
+    "abbrev": "RSK"
+  },
+  "テレビせとうち": {
+    "type": "地上波",
+    "area": "中国",
+    "group": "テレビ東京",
+    "abbrev": "TSC"
+  },
+  "岡山放送": {
+    "type": "地上波",
+    "area": "中国",
+    "group": "フジテレビ",
+    "abbrev": "OHK"
+  },
+  "中国放送": {
+    "type": "地上波",
+    "area": "中国",
+    "group": "TBS",
+    "abbrev": "RCC"
+  },
+  "広島テレビ": {
+    "type": "地上波",
+    "area": "中国",
+    "group": "日本テレビ",
+    "abbrev": "HTV"
+  },
+  "広島ホームテレビ": {
+    "type": "地上波",
+    "area": "中国",
+    "group": "テレビ朝日",
+    "abbrev": "HOME"
+  },
+  "テレビ新広島": {
+    "type": "地上波",
+    "area": "中国",
+    "group": "フジテレビ",
+    "abbrev": "TSS"
+  },
+  "テレビ山口": {
+    "type": "地上波",
+    "area": "中国",
+    "group": "TBS",
+    "abbrev": "tys"
+  },
+  "山口放送": {
+    "type": "地上波",
+    "area": "中国",
+    "group": "日本テレビ",
+    "abbrev": "KRY"
+  },
+  "山口朝日放送": {
+    "type": "地上波",
+    "area": "中国",
+    "group": "テレビ朝日",
+    "abbrev": "yab"
+  },
+  "四国放送": {
+    "type": "地上波",
+    "area": "四国",
+    "group": "日本テレビ",
+    "abbrev": "JRT"
+  },
+  "南海放送": {
+    "type": "地上波",
+    "area": "四国",
+    "group": "日本テレビ",
+    "abbrev": "RNB"
+  },
+  "愛媛朝日テレビ": {
+    "type": "地上波",
+    "area": "四国",
+    "group": "テレビ朝日",
+    "abbrev": "eat"
+  },
+  "あいテレビ": {
+    "type": "地上波",
+    "area": "四国",
+    "group": "TBS",
+    "abbrev": "ITV"
+  },
+  "テレビ愛媛": {
+    "type": "地上波",
+    "area": "四国",
+    "group": "フジテレビ",
+    "abbrev": "EBC"
+  },
+  "高知放送": {
+    "type": "地上波",
+    "area": "四国",
+    "group": "日本テレビ",
+    "abbrev": "RKC"
+  },
+  "テレビ高知": {
+    "type": "地上波",
+    "area": "四国",
+    "group": "TBS",
+    "abbrev": "KUTV"
+  },
+  "高知さんさんテレビ": {
+    "type": "地上波",
+    "area": "四国",
+    "group": "フジテレビ",
+    "abbrev": "KSS"
+  },
+  "九州朝日放送": {
+    "type": "地上波",
+    "area": "九州",
+    "group": "テレビ朝日",
+    "abbrev": "KBC"
+  },
+  "福岡放送": {
+    "type": "地上波",
+    "area": "九州",
+    "group": "日本テレビ",
+    "abbrev": "FBS"
+  },
+  "九州放送": {
+    "type": "地上波",
+    "area": "九州",
+    "group": "テレビ東京",
+    "abbrev": "TVQ"
+  },
+  "テレビ西日本": {
+    "type": "地上波",
+    "area": "九州",
+    "group": "フジテレビ",
+    "abbrev": "TNC"
+  },
+  "サガテレビ": {
+    "type": "地上波",
+    "area": "九州",
+    "group": "フジテレビ",
+    "abbrev": "STS"
+  },
+  "長崎放送": {
+    "type": "地上波",
+    "area": "九州",
+    "group": "TBS",
+    "abbrev": "NBC"
+  },
+  "長崎国際テレビ": {
+    "type": "地上波",
+    "area": "九州",
+    "group": "日本テレビ",
+    "abbrev": "NIB"
+  },
+  "長崎文化放送": {
+    "type": "地上波",
+    "area": "九州",
+    "group": "テレビ朝日",
+    "abbrev": "NCC"
+  },
+  "テレビ長崎": {
+    "type": "地上波",
+    "area": "九州",
+    "group": "フジテレビ",
+    "abbrev": "KTN"
+  },
+  "熊本放送": {
+    "type": "地上波",
+    "area": "九州",
+    "group": "TBS",
+    "abbrev": "RKK"
+  },
+  "くまもと県民テレビ": {
+    "type": "地上波",
+    "area": "九州",
+    "group": "日本テレビ",
+    "abbrev": "KKT"
+  },
+  "熊本朝日放送": {
+    "type": "地上波",
+    "area": "九州",
+    "group": "テレビ朝日",
+    "abbrev": "KAB"
+  },
+  "テレビ熊本": {
+    "type": "地上波",
+    "area": "九州",
+    "group": "フジテレビ",
+    "abbrev": "TKU"
+  },
+  "大分放送": {
+    "type": "地上波",
+    "area": "九州",
+    "group": "TBS",
+    "abbrev": "OBS"
+  },
+  "テレビ大分": {
+    "type": "地上波",
+    "area": "九州",
+    "group": "日本テレビフジテレビ",
+    "abbrev": "TOS"
+  },
+  "大分朝日放送": {
+    "type": "地上波",
+    "area": "九州",
+    "group": "テレビ朝日",
+    "abbrev": "OAB"
+  },
+  "テレビ宮崎": {
+    "type": "地上波",
+    "area": "九州",
+    "group": "クロスネット",
+    "abbrev": "UMK"
+  },
+  "宮崎放送": {
+    "type": "地上波",
+    "area": "九州",
+    "group": "TBS",
+    "abbrev": "MRT"
+  },
+  "南日本放送": {
+    "type": "地上波",
+    "area": "九州",
+    "group": "TBS",
+    "abbrev": "MBC"
+  },
+  "鹿児島読売テレビ": {
+    "type": "地上波",
+    "area": "九州",
+    "group": "日本テレビ",
+    "abbrev": "KYT"
+  },
+  "鹿児島放送": {
+    "type": "地上波",
+    "area": "九州",
+    "group": "テレビ朝日",
+    "abbrev": "KKB"
+  },
+  "鹿児島テレビ": {
+    "type": "地上波",
+    "area": "九州",
+    "group": "フジテレビ",
+    "abbrev": "KTS"
+  },
+  "琉球放送": {
+    "type": "地上波",
+    "area": "沖縄",
+    "group": "TBS",
+    "abbrev": "RBC"
+  },
+  "琉球朝日放送": {
+    "type": "地上波",
+    "area": "沖縄",
+    "group": "テレビ朝日",
+    "abbrev": "QAB"
+  },
+  "沖縄テレビ": {
+    "type": "地上波",
+    "area": "沖縄",
+    "group": "フジテレビ",
+    "abbrev": "OTV"
+  }
+};
